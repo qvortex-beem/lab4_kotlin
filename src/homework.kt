ad6820359763893b0b9d2c570b6996313d2c3b85
A-39GFp-78 <A-39GFp-78@DESKTOP-76J6RFA>
2025-11-02 15:48:33 +0300

    выполнил два задания дзшки

---
+++ src/homework.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //задание 1
+//    showGameLogo()
+//    showGameLogo()
+
+    //задание 2
+//    showEnemy("Дракон", 70)
+//    showEnemy("огр", 15)
+}
+
+// задание 1
+//fun showGameLogo() = println("=== EPIC QUEST ===")
+
+// задание 2
+//fun showEnemy(name: String, level: Int) = println("Враг: $name уровень: $level")
+
+//
